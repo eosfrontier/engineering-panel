f7147f79a48514807f36fb6fe3ab29ede6ba7a38
Willem Monsuwe <wilem@monsuwe.nl>
2018-06-19 19:12:29 +0000

    Moved files

--- connectorgame/audio.h
+++ connectorgame/audio.h
@@ -7,7 +7,7 @@
 
 #define PCM_BUFSIZE     (4410*2*2)
 
-#define PCM_PATH        "/home/pi/connectorgame/audio/"
+#define PCM_PATH        "/home/pi/shuttle-panels/audio/"
 
 int init_audio(void);
 int fini_audio(void);
