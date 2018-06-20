7d49de38900387d1b7517e217ac6c06363fa8d7f
Willem Monsuwe <willem@monsuwe.nl>
2018-06-20 12:52:22 +0200

    Mastermind eerste versie

---
+++ connectorgame/connectors.h
@@ -0,0 +1,24 @@
+
+static char *c_colors = 
+	"BZGYR"
+	"GYRBZ"
+	"ZRYGB"
+	"YBZRG"
+	"RGYBZ"
+	"BRGZY"
+	"YZRGB"
+	"GBZYR"
+	"RYBGZ"
+	"ZGRBY"
+
+	"RGYZB"
+	"GZBYR"
+	"ZRGBY"
+	"BYRGZ"
+	"YGBZR"
+	"RBZGY"
+	"ZRGYB"
+	"GYZBR"
+	"BZGRY"
+	"RGYZB";
+
