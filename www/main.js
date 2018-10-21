885f017d422f08f4c04a829b15f89abc8a472733
Willem Monsuwe <willem@monsuwe.nl>
2018-10-21 19:50:52 +0200

    Break/repair button v0.1

--- www/main.js
+++ www/main.js
@@ -27,11 +27,11 @@ function create_display(colors)
         'Y':'yellow'
     }
     var html = []
-    html.push('<tr id="switches" class="switches"><td colspan="5"></td>')
+    html.push('<tr id="switches" class="switches"><td colspan="5"><div class="button" myval="0.5">Break</div><div class="button" myval="1.0">Fix</div></td>')
     for (var s = 0; s < 3; s++) {
-        html.push('<td class="switch switch_',s,'"><div><div class="top"></div><div class="bar"></div></div></td>');
+        html.push('<td class="switch switch_',s,'"><div><div class="top"></div><div class="bar"></div></div></td>')
     }
-    html.push('<td colspan="5"></td></tr>')
+    html.push('<td colspan="5"><div id="repairlevel"><div class="full"></div><div class="empty"></div></div></td></tr>')
     html.push('<tr class="connectors">')
     for (var c = 0; c < 100; c++) {
         if (c % 10 == 5) html.push('<td class="center" colspan="3"></td>')
@@ -44,7 +44,20 @@ function create_display(colors)
     for (var c = 0; c < colors.length; c++) {
         $(connectors[map_connector(c)]).addClass(colormap[colors[c]])
     }
+    $('#switches div.button').click(set_repair)
     reload()
+}
+
+function set_repair()
+{
+    var repairlevel = $(this).attrib('myval')
+    $(this).addClass('clicked')
+    $.post('set_repairlevel.php', { repairlevel: repairlevel }, clicked_repair)
+}
+
+function clicked_repair()
+{
+    $('#switched div.button').removeClass('clicked')
 }
 
 function reload()
@@ -58,7 +71,7 @@ function reload()
     })
 }
 
-var lastts = 0;
+var lastts = 0
 
 function display_connections(json)
 {
@@ -76,6 +89,8 @@ function display_connections(json)
         $('#switches .switch_'+t+' div.top').height(100*(1.0-json.turbines[t])+'%')
         $('#switches .switch_'+t+' div.bar').height(100*json.turbines[t]+'%')
     }
+    $('#repairlevel .full').width((100*(json.repairlevel))+'%')
+    $('#repairlevel .empty').width((100*(1.0-json.repairlevel))+'%')
     var connectors = $('#connectors td.connector').removeClass('connected').get()
     for (var c = 0; c < json.connections.length; c++) {
         var conn = json.connections[c]
