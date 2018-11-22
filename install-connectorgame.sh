6998e63df7d440e2a1aec9f572cbdf832906ef5b
Willem Monsuwe <wilem@monsuwe.nl>
2018-11-22 15:50:38 +0000

    Script om van kale PI te installeren

---
+++ install-connectorgame.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get -q -y update
+sudo apt-get -q -y install wiringpi scons git lighttpd php7.0-fpm libasound2-dev
+
+git checkout git://github.com/willemm/shuttle-panels.git /home/pi/shuttle-panels
+
+sudo cat /home/pi/shuttle-panels/lighttpd.conf >> /etc/lighttpd/lighttpd.conf
+sudo cat /home/pi/shuttle-panels/fpm-www.conf >> /etc/php/7.0/fpm/pool.d/www.conf
+
+make -C /home/pi/shuttle-panels/connectorgame install
