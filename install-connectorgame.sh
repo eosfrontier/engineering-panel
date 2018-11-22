b85f03e8c2da0e6426325522e6e67738c991b5c4
Willem Monsuwe <willem@monsuwe.nl>
2018-11-22 19:57:38 +0000

    Installscript fixjes

--- install-connectorgame.sh
+++ install-connectorgame.sh
@@ -5,14 +5,14 @@ sudo apt-get -q -y install wiringpi scons git lighttpd php7.0-fpm libasound2-dev
 
 sudo systemctl stop lighttpd php7.0-fpm
 
-git clone git://github.com/willemm/shuttle-panels.git /home/pi/shuttle-panels
+git clone --recursive git://github.com/willemm/shuttle-panels.git /home/pi/shuttle-panels
 
-sudo chown pi.pi /var/log/lighttpd
+sudo chown -R pi.pi /var/log/lighttpd
 
-cat /home/pi/shuttle-panels/lighttpd.conf | sudo tee -s /etc/lighttpd/lighttpd.conf
-cat /home/pi/shuttle-panels/fpm-www.conf | sudo tee -s /etc/php/7.0/fpm/pool.d/www.conf
 sudo cp /home/pi/shuttle-panels/asound.conf /etc/asound.conf
 sudo cp /home/pi/shuttle-panels/boot-config.txt /boot/config.txt
+sudo cp /home/pi/shuttle-panels/lighttpd.conf /etc/lighttpd/lighttpd.conf
+cat /home/pi/shuttle-panels/fpm-www.conf | sudo tee -a /etc/php/7.0/fpm/pool.d/www.conf
 
 make -C /home/pi/shuttle-panels/connectorgame install
 
