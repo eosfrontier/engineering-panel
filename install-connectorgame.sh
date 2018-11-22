75abdec7419d06a32b3180d66d99d957a9d86323
Willem Monsuwe <wilem@monsuwe.nl>
2018-11-22 16:36:41 +0000

    Pi setup fixes

--- install-connectorgame.sh
+++ install-connectorgame.sh
@@ -3,7 +3,7 @@
 sudo apt-get -q -y update
 sudo apt-get -q -y install wiringpi scons git lighttpd php7.0-fpm libasound2-dev
 
-sudo systemctl stop lighttpd
+sudo systemctl stop lighttpd php7.0-fpm
 
 git clone git://github.com/willemm/shuttle-panels.git /home/pi/shuttle-panels
 
@@ -14,6 +14,6 @@ cat /home/pi/shuttle-panels/fpm-www.conf | sudo tee -s /etc/php/7.0/fpm/pool.d/w
 sudo cp /home/pi/shuttle-panels/asound.conf /etc/asound.conf
 sudo cp /home/pi/shuttle-panels/boot-config.txt /boot/config.txt
 
-sudo systemctl start lighttpd
+sudo systemctl start lighttpd php7.0-fpm
 
 make -C /home/pi/shuttle-panels/connectorgame install
