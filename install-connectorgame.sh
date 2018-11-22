dd70516fac5051ebcef4cef5144dc422957ee1f7
Willem Monsuwe <wilem@monsuwe.nl>
2018-11-22 16:05:54 +0000

    Meer pi setup dingen

--- install-connectorgame.sh
+++ install-connectorgame.sh
@@ -7,5 +7,7 @@ git checkout git://github.com/willemm/shuttle-panels.git /home/pi/shuttle-panels
 
 sudo cat /home/pi/shuttle-panels/lighttpd.conf >> /etc/lighttpd/lighttpd.conf
 sudo cat /home/pi/shuttle-panels/fpm-www.conf >> /etc/php/7.0/fpm/pool.d/www.conf
+sudo cp /home/pi/shuttle-panels/asound.conf /etc/asound.conf
+sudo cp /home/pi/shuttle-panels/boot-config.txt /boot/config.txt
 
 make -C /home/pi/shuttle-panels/connectorgame install
